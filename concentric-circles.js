7b8e05a81d33e2e401632e5c92628b05e3891358
WalnutsAreCool <claadam@nuevaschool.org>
2016-12-09 09:29:35 -0800

    Classadoo autocommit

--- concentric-circles.js
+++ concentric-circles.js
@@ -12,11 +12,10 @@ function nSidesL(n,l) {
 //nSidesL(90, 4)
 //nSidesL(90, 3)
 
-for (i=1; i<10
-     i++) {
+for (i=1; i<10; i++) {
   nSidesL(90,10-i)
   nDegreesLeft(90)
-  goForward(15)
+  goInvisible(15)
   nDegreesRight(90)
 }
 //nSidesL(90, 2)
